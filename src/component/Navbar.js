287061fb60f6be7dd583880b1019957fb5a55483
sigitwhyu <sigitwahyu.mutu@gmail.com>
2020-08-18 14:49:41 +0700

    update script route-link

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,10 +5,10 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return (
-                <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="/">My React</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand" href="/">My React</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
